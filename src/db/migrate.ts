b8eaf3838327d044cfa85bb1662042563cd1b9dc
Lucas Johann <gabif.johann@gmail.com>
2024-09-15 19:52:21 -0300

    add: a customer log to migrations

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk'
 import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
@@ -8,6 +9,8 @@ const connection = postgres(env.DB_URL, { max: 1 })
 const dbConnection = drizzle(connection)
 
 await migrate(dbConnection, { migrationsFolder: 'drizzle' })
+
+console.log(chalk.greenBright('Migrations applied succesfully!'))
 
 await connection.end()
 
